2aa1c8cab1f1a5e633e02d8e667438117a7814db
Max Bigras <mbigras22@gmail.com>
2022-03-05 12:42:26 -0800

    Add time metadata (#3)
    * Add time metadata
    
    * Add start_time_human

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 import os
 
 import flask
+import arrow
 
 app = flask.Flask(__name__)
 app.config["app"] = os.environ.get("APP", "app")
 app.config["version"] = os.environ.get("VERSION", "0")
 app.config["commit"] = os.environ.get("COMMIT", "00e8872b526c45e6272216ca57be60c00705b53f")
+app.config["build_time"] = arrow.get(os.environ.get("BUILD_TIME", "2022-03-05 19:12:07+00:00")).to("utc")
+app.config["start_time"] = arrow.utcnow()
 app.config["env"] = os.environ.get("ENV", "prod")
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 @app.route("/")
 def up():
+    request_time = arrow.utcnow()
     return flask.jsonify(
         app=app.config["app"],
         version=app.config["version"],
         commit=app.config["commit"],
         env=app.config["env"],
+        build_time=app.config["build_time"].format(arrow.FORMAT_RFC3339),
+        start_time=app.config["start_time"].format(arrow.FORMAT_RFC3339),
+        start_time_human=app.config["start_time"].humanize(),
+        request_time=request_time.format(arrow.FORMAT_RFC3339),
         message="hello world!",
     )
---
+++ utcnow.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Print the current time in UTC in RFC3339 format.
+date -u +'%Y-%m-%d %H:%M:%S+00:00'

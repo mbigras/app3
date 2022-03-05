cc2e748eaa57cd80845d6343a972cb9cfa11cd70
Max Bigras <mbigras22@gmail.com>
2022-03-05 01:07:54 -0800

    Add GitHub actions take 9

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,15 @@ RUN apk add --update --no-cache python3==3.9.5-r2 \
 COPY requirements.txt ./
 RUN /app/venv/bin/pip install -r requirements.txt
 COPY entrypoint.sh app.py ./
+
+ARG APP
+ARG VERSION
+ARG COMMIT
+ARG ENV
+
+ENV APP=$APP
+ENV VERSION=$VERSION
+ENV COMMIT=$COMMIT
+ENV ENV=$ENV
+
 ENTRYPOINT ["/app/entrypoint.sh"]
--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import flask
 
 app = flask.Flask(__name__)
+app.config["app"] = os.environ.get("APP", "app")
+app.config["version"] = os.environ.get("VERSION", "0")
+app.config["commit"] = os.environ.get("COMMIT", "00e8872b526c45e6272216ca57be60c00705b53f")
+app.config["env"] = os.environ.get("ENV", "prod")
 
 @app.route("/")
 def up():
